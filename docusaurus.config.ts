c46306906cef95ebf679517054e47fed99512458
melissaji1998 <melissaji1998@gmail.com>
2024-10-17 23:26:13 +0530

    Netflify Updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://docs.skidiolabs.com',
+  url: 'https://docusaurus-2.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
+
+   // Netlify Deployment Config
+   trailingSlash: false, // Disable trailing slash
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
