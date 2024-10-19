f89ec53b20095a9d5621660c63df3f48e315c6c4
melissaji1998 <melissaji1998@gmail.com>
2024-10-20 01:15:35 +0530

    added icon and header, footer updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,3 +1,155 @@
+// import {themes as prismThemes} from 'prism-react-renderer';
+// import type {Config} from '@docusaurus/types';
+// import type * as Preset from '@docusaurus/preset-classic';
+
+// const config: Config = {
+//   title: 'SkidioLabs',
+//   tagline: 'Build stunning AI images',
+//   favicon: 'img/favicon.ico',
+
+//   // Set the production url of your site here
+//   url: 'https://docusaurus-2.netlify.app',
+//   // Set the /<baseUrl>/ pathname under which your site is served
+//   // For GitHub pages deployment, it is often '/<projectName>/'
+//   baseUrl: '/',
+
+//    // Netlify Deployment Config
+//   trailingSlash: false, // Disable trailing slash
+
+//   // GitHub pages deployment config.
+//   // If you aren't using GitHub pages, you don't need these.
+//   organizationName: 'melissaji1998', // Usually your GitHub org/user name.
+//   projectName: 'docusaurus', // Usually your repo name.
+
+//   onBrokenLinks: 'throw',
+//   onBrokenMarkdownLinks: 'warn',
+
+//   // Even if you don't use internationalization, you can use this field to set
+//   // useful metadata like html lang. For example, if your site is Chinese, you
+//   // may want to replace "en" with "zh-Hans".
+//   i18n: {
+//     defaultLocale: 'en',
+//     locales: ['en'],
+//   },
+
+//   presets: [
+//     [
+//       'classic',
+//       {
+//         docs: {
+//           sidebarPath: './sidebars.ts',
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//         },
+//         blog: {
+//           showReadingTime: true,
+//           feedOptions: {
+//             type: ['rss', 'atom'],
+//             xslt: true,
+//           },
+//           // Please change this to your repo.
+//           // Remove this to remove the "edit this page" links.
+//           editUrl:
+//             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+//           // Useful options to enforce blogging best practices
+//           onInlineTags: 'warn',
+//           onInlineAuthors: 'warn',
+//           onUntruncatedBlogPosts: 'warn',
+//         },
+//         theme: {
+//           customCss: './src/css/custom.css',
+//         },
+//       } satisfies Preset.Options,
+//     ],
+//   ],
+
+//   themeConfig: {
+//     colorMode: {
+//       defaultMode: 'dark',
+//       disableSwitch: false,
+//       respectPrefersColorScheme: false,
+//     },
+//     // Replace with your project's social card
+//     image: 'img/docusaurus-social-card.jpg',
+//     navbar: {
+//       // title: 'My Site',
+//       logo: {
+//         alt: 'SkidioLabs',
+//         src: '/img/skidiolabs.png',
+//         href: 'https://www.skidiolabs.com',
+//       },
+//       items: [
+//         {
+//           type: 'docSidebar',
+//           sidebarId: 'tutorialSidebar',
+//           position: 'left',
+//           label: 'Tutorial',
+//         },
+//         {to: '/blog', label: 'Blog', position: 'left'},
+//         {
+//           href: 'https://github.com/facebook/docusaurus',
+//           label: 'GitHub',
+//           position: 'right',
+//         },
+//       ],
+//     },
+//     footer: {
+//       style: 'dark',
+//       links: [
+//         {
+//           title: 'Docs',
+//           items: [
+//             {
+//               label: 'Tutorial',
+//               to: '/docs/intro',
+//             },
+//           ],
+//         },
+//         {
+//           title: 'Community',
+//           items: [
+//             {
+//               label: 'Stack Overflow',
+//               href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+//             },
+//             {
+//               label: 'Discord',
+//               href: 'https://discordapp.com/invite/docusaurus',
+//             },
+//             {
+//               label: 'Twitter',
+//               href: 'https://twitter.com/docusaurus',
+//             },
+//           ],
+//         },
+//         {
+//           title: 'More',
+//           items: [
+//             {
+//               label: 'Blog',
+//               to: '/blog',
+//             },
+//             {
+//               label: 'GitHub',
+//               href: 'https://github.com/facebook/docusaurus',
+//             },
+//           ],
+//         },
+//       ],
+//       copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+//     },
+//     prism: {
+//       theme: prismThemes.github,
+//       darkTheme: prismThemes.dracula,
+//     },
+//   } satisfies Preset.ThemeConfig,
+// };
+
+// export default config;
+
+
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
@@ -5,28 +157,19 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'SkidioLabs',
   tagline: 'Build stunning AI images',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/split_black.ico',
 
-  // Set the production url of your site here
   url: 'https://docusaurus-2.netlify.app',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-   // Netlify Deployment Config
-  trailingSlash: false, // Disable trailing slash
+  trailingSlash: false,
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'melissaji1998', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'melissaji1998',
+  projectName: 'docusaurus',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -38,26 +181,10 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false, // Disable the blog plugin
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -71,13 +198,12 @@ const config: Config = {
       disableSwitch: false,
       respectPrefersColorScheme: false,
     },
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
       logo: {
         alt: 'SkidioLabs',
-        src: 'skidiolabs.png',
+        src: '/img/skidiolabs.png',
+        href: 'https://skidiolabs.com',
       },
       items: [
         {
@@ -86,64 +212,34 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
       ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  // Add this new section to include the chatbot script
+  scripts: [
+    {
+      src: 'https://agentivehub.com/production.bundle.min.js',
+      async: true,
+      'data-widget-id': 'c7d4c4ff-0f97-4d22-8893-337c99955922',
+      onload: `
+        if (!document.getElementById('root')) {
+          var root = document.createElement('div');
+          root.id = 'root';
+          document.body.appendChild(root);
+        }
+        if (window.myChatWidget && typeof window.myChatWidget.load === 'function') {
+          window.myChatWidget.load({
+            id: 'c7d4c4ff-0f97-4d22-8893-337c99955922',
+          });
+        }
+      `,
+    },
+  ],
 };
 
 export default config;
